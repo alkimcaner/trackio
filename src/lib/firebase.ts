09248d83c2851d2e531c5e18135feadec72fb824
Alkım Caner Soydan <alkimcanersoydan@gmail.com>
2023-08-14 19:20:26 +0300

    refactor: hide firebase config

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCwzOnabWiNdRnVjSmFCM21LvN_Fqtbxrk",
-  authDomain: "gamingstore-f968a.firebaseapp.com",
-  projectId: "gamingstore-f968a",
-  storageBucket: "gamingstore-f968a.appspot.com",
-  messagingSenderId: "374879030768",
-  appId: "1:374879030768:web:13f3a3e80524a703d46135",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
