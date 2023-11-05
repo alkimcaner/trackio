79fc04fd325c05c0f61a6efbe19df6dc03a9e62f
Alkım Caner Soydan <alkimcanersoydan@gmail.com>
2023-11-05 18:02:40 +0300

    chore: update next.js

--- src/components/GameSlider.tsx
+++ src/components/GameSlider.tsx
@@ -9,34 +9,32 @@ import GameCard from "./GameCard";
 
 export default function GameSlider({ gamesData }: any) {
   return (
-    <div>
-      <Swiper
-        modules={[Navigation, Autoplay]}
-        spaceBetween={16}
-        slidesPerView={6}
-        autoplay={{
-          delay: 5000,
-          disableOnInteraction: false,
-        }}
-        navigation
-        breakpoints={{
-          360: {
-            slidesPerView: 2,
-          },
-          768: {
-            slidesPerView: 4,
-          },
-          1024: {
-            slidesPerView: 6,
-          },
-        }}
-      >
-        {gamesData?.map((gameData: any) => (
-          <SwiperSlide key={gameData.id}>
-            <GameCard gameData={gameData} />
-          </SwiperSlide>
-        ))}
-      </Swiper>
-    </div>
+    <Swiper
+      modules={[Navigation, Autoplay]}
+      spaceBetween={16}
+      slidesPerView={6}
+      autoplay={{
+        delay: 5000,
+        disableOnInteraction: false,
+      }}
+      navigation
+      breakpoints={{
+        360: {
+          slidesPerView: 2,
+        },
+        768: {
+          slidesPerView: 4,
+        },
+        1024: {
+          slidesPerView: 6,
+        },
+      }}
+    >
+      {gamesData?.map((gameData: any) => (
+        <SwiperSlide key={gameData.id}>
+          <GameCard gameData={gameData} />
+        </SwiperSlide>
+      ))}
+    </Swiper>
   );
 }
