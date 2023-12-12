f9d6dec49f4fa71786c8d4f96900b95b056c1344
Alkım Caner Soydan <alkimcanersoydan@gmail.com>
2023-12-12 16:54:04 +0300

    refactor: change game sliders to grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,19 @@
-import GameSlider from "@/components/GameSlider";
+import GameGrid from "@/components/GameGrid";
 
 export default async function Home() {
   const bestGames = await fetch(`${process.env.NEXTAUTH_URL}/api/games`, {
     method: "POST",
-    body: "fields *,cover.*; where total_rating_count > 100; sort total_rating desc;",
+    body: "fields *,cover.*; where total_rating_count > 100; sort total_rating desc; limit 6;",
   }).then((res) => res.json());
 
   const ps4Games = await fetch(`${process.env.NEXTAUTH_URL}/api/games`, {
     method: "POST",
-    body: "fields *,cover.*; where total_rating_count > 100 & category = 0 & platforms = {48}; sort total_rating desc;",
+    body: "fields *,cover.*; where total_rating_count > 100 & category = 0 & platforms = {48}; sort total_rating desc; limit 6;",
   }).then((res) => res.json());
 
   const nsGames = await fetch(`${process.env.NEXTAUTH_URL}/api/games`, {
     method: "POST",
-    body: "fields *,cover.*; where total_rating_count > 100 & category = 0 & platforms = {130}; sort total_rating desc;",
+    body: "fields *,cover.*; where total_rating_count > 100 & category = 0 & platforms = {130}; sort total_rating desc; limit 6;",
   }).then((res) => res.json());
 
   return (
@@ -21,15 +21,15 @@ export default async function Home() {
       <section className="mx-auto flex w-full max-w-7xl flex-col gap-8 p-8">
         <div>
           <h1 className="mb-4 text-lg">Best Games</h1>
-          <GameSlider games={bestGames} />
+          <GameGrid games={bestGames} />
         </div>
         <div>
           <h1 className="mb-4 text-lg">Best Playstation 4 Exclusives</h1>
-          <GameSlider games={ps4Games} />
+          <GameGrid games={ps4Games} />
         </div>
         <div>
           <h1 className="mb-4 text-lg">Best Nintendo Switch Exclusives</h1>
-          <GameSlider games={nsGames} />
+          <GameGrid games={nsGames} />
         </div>
       </section>
     </main>
